d4333f7d93523057a93274e48a956b96898f1258
felixwalberg <122999576+felixwalberg@users.noreply.github.com>
2023-10-24 11:10:29 -0400

    Fixed bugs in datetime_convert.java
    Added a semicolon to the dateStr initialization and changed the formattedDate to a String type

--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -5,7 +5,7 @@ import java.time.format.DateTimeFormatter;
 public class datetime_convert {
     public static void main(String[] args) {
         // Creating date string
-        String dateStr = "2022-03-17 10:45:30"
+        String dateStr = "2022-03-17 10:45:30";
 
         // Creating DateTimeFormatter object to set date time format
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
@@ -14,7 +14,7 @@ public class datetime_convert {
         LocalDateTime dateObj = LocalDateTime.parse(dateStr, formatter);
 
         // Creating a String out of the LocalDateTime object
-        int formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
+        String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
 
         // Printing formatted date
         System.out.println(formattedDate);
